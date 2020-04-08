9852b0b50066348df21818495915d55d76af5145
Alexander Zamirouski <studiaformanta@yandex.ru>
2020-04-08 07:09:38 -0700

    App component cleared

--- src/App/App.js
+++ src/App/App.js
@@ -1,43 +1,13 @@
 import React from 'react';
 
 import FilterSection from '../FilterSection';
-import TextInput from '../TextInput';
-import Button, { buttonTypes } from '../Button';
-import { DropdownWithSearch, DropdownWithAvatars } from '../Dropdown';
 
 import './App.css';
 
 function App() {
-  const cars = [
-    { id: 'opel', label: 'Opel' },
-    { id: 'volvo', label: 'Volvo' },
-    { id: 'bmv', label: 'BMW' }
-  ];
-
-  const buyers = [
-    { id: 'ira', label: 'Ira', avatar: 'example/source/to/avatar' },
-    { id: 'petia', label: 'Petia', avatar: 'example/source/to/avatar' }
-  ];
-
   return (
     <div className="App">
-      <FilterSection
-        cols={[
-          <>
-            <DropdownWithSearch options={cars} placeholder="Select buyer" />
-            <DropdownWithAvatars options={buyers} placeholder={{ label: 'Select car' }} />
-          </>,
-          <>
-            <TextInput name="example1" />
-            <TextInput name="example2" />
-            <TextInput name="example3" />
-          </>,
-          <>
-            <Button type={buttonTypes.PRIMARY} text="Reset" />
-            <Button type={buttonTypes.SECONDARY} text="Filter" />
-          </>
-        ]}
-      />
+      <FilterSection />
     </div>
   );
 }
--- src/FilterSection/FilterSection.js
+++ src/FilterSection/FilterSection.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
+import TextInput from '../TextInput';
+import Button, { buttonTypes } from '../Button';
+import { DropdownWithSearch, DropdownWithAvatars } from '../Dropdown';
+
 import './FilterSection.scss';
 
-const FilterSection = ({ cols }) => {
+const FilterSection = () => {
+  const cars = [
+    { id: 'opel', label: 'Opel' },
+    { id: 'volvo', label: 'Volvo' },
+    { id: 'bmv', label: 'BMW' }
+  ];
+
+  const buyers = [
+    { id: 'ira', label: 'Ira', avatar: 'example/source/to/avatar' },
+    { id: 'petia', label: 'Petia', avatar: 'example/source/to/avatar' }
+  ];
+
   return (
     <section className="filter-section">
-      {cols.map(col => (
-        <div className="filter-section__raw">
-          {col}
-        </div>
-      ))}
+      <div className="filter-section__raw">
+        <DropdownWithSearch options={cars} placeholder="Select buyer" />
+        <DropdownWithAvatars options={buyers} placeholder={{ label: 'Select car' }} />
+      </div>
+      <div className="filter-section__raw">
+        <TextInput name="example1" />
+        <TextInput name="example2" />
+        <TextInput name="example3" />
+      </div>
+      <div className="filter-section__raw">
+        <Button type={buttonTypes.PRIMARY} text="Reset" />
+        <Button type={buttonTypes.SECONDARY} text="Filter" />
+      </div>
     </section>
   );
 };
